ba5ef261940c855e4ac20ab06a7c95a031d0ba41
Hendrick <78098509+Xhand98@users.noreply.github.com>
2024-08-05 17:59:31 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import os
+
+def read(file_name):
+    with open(os.path.join(os.path.dirname(__file__), file_name), encoding='utf-8') as f:
+        return f.read()
+
+setup(
+    name='steam_api',  # Replace with your library name
+    version='0.7',  # Starting version of your library
+    description='A library for interacting with the Steam API',  # Brief description
+    author='Hendrick',  # Your name
+    author_email='hendrickherrera9@.com',  # Your email address
+    url='https://github.com/Xhand98/yourrepository',  # URL to your project
+    packages=find_packages(),  # Automatically find packages in your project
+    install_requires=[
+        'httpx',  # List of dependencies
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # License type
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Minimum Python version required
+)
